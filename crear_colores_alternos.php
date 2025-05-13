a8458ec2cf754d5ef68bc2b6d47eb5af601ed674
Jose Candido <josecandidop@hotmail.com>
2025-05-13 15:06:34 -0600

    Crear script para obtener estilos base para -txt y bg- según la paleta de colores en Json

---
+++ crear_colores_alternos.php
@@ -0,0 +1,32 @@
+<?php
+
+$datos = file_get_contents("2025 0513 1344 - Cimsur-Rojo2.json");
+$json_data = json_decode('$datos');
+
+//echo "<br><br>";
+//var_dump(json_decode($datos));
+
+$nombreIni = "";
+$nombreFin = "";
+
+$coloresTxt = '';
+$coloresBg = '';
+
+echo "<br><br>";
+
+foreach (json_decode($datos, true) as $key => $value) {
+    if ($nombreIni != substr($key, 6, 3)) {
+        $coloresTxt .= "<br/><br/>";
+        $coloresBg .= "<br/><br/>";
+    }
+
+    $nombreIni = substr($key, 6, 3);
+    $nombreFin = substr($key, -3);
+
+    $coloresTxt .= ".txt-$nombreIni-$nombreFin {color:$value;}<br/>";
+    $coloresBg .= ".bg-$nombreIni-$nombreFin {background-color:$value;}<br/>";
+}
+
+
+echo $coloresTxt . "<br/><br/>";
+echo $coloresBg;
